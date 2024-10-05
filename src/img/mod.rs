6f5771f92ede2d4f6ea8892ea7ab119923f801f1
Bo Lu <bo@plenoinc.com>
2024-10-04 20:32:46 -0700

    wip

--- src/img/mod.rs
+++ src/img/mod.rs
@@ -2,20 +2,19 @@ use image::{DynamicImage, ImageOutputFormat};
 use serde::Deserialize;
 use std::fs;
 use std::io::Cursor;
+use warp::hyper::body::Bytes;
 
-// Struct to parse query parameters (width and height).
 #[derive(Debug, Deserialize)]
 pub struct ThumbnailParams {
     pub width: u32,
     pub height: u32,
 }
 
-// Function to create a thumbnail.
 pub fn create_thumbnail(
     image_path: &str,
     width: u32,
     height: u32,
-) -> Result<warp::hyper::body::Bytes, std::io::Error> {
+) -> Result<Bytes, std::io::Error> {
     // Check if the image file exists and is accessible.
     if !fs::metadata(image_path).is_ok() {
         return Err(std::io::Error::new(
@@ -23,8 +22,14 @@ pub fn create_thumbnail(
             "Image file not found",
         ));
     }
+
     // Open and load the image from the file path.
-    let img = image::open(&image_path).expect("Failed to open image file");
+    let img = image::open(&image_path).map_err(|e| {
+        std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Failed to open image file: {}", e),
+        )
+    })?;
 
     // Resize the image to the given dimensions.
     let thumbnail = img.thumbnail(width, height);
@@ -33,8 +38,13 @@ pub fn create_thumbnail(
     let mut buffer = Cursor::new(Vec::new());
     thumbnail
         .write_to(&mut buffer, ImageOutputFormat::Png)
-        .expect("Failed to write image to buffer");
+        .map_err(|e| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Failed to write image to buffer: {}", e),
+            )
+        })?;
 
     // Convert the buffer to a warp-compatible response.
-    Ok(warp::hyper::body::Bytes::from(buffer.into_inner()))
+    Ok(Bytes::from(buffer.into_inner()))
 }
